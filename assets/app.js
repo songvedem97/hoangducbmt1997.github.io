594d0486b3188daa5579ebc217304b5757461f3e
songvedem <56100101+hoangducbmt1997@users.noreply.github.com>
2021-09-17 22:58:41 +0700

    update new version

--- assets/app.js
+++ assets/app.js
@@ -1,23 +1,24 @@
-window.onload = async ()=>{
+// load dữ liệu
+window.onload = async () => {
 	$body = $("body");
 	$body.addClass("loading");
 	getSong();
 	await getData();
 	$body.removeClass("loading");
 }
+// biến tương tác
 const musicContent = document.querySelector(".row");
 const avatar = document.querySelector(".avatar img");
 const name = document.querySelector(".music-play .name");
 const creator = document.querySelector(".music-play .creator");
-const btnPlay = document.querySelector(".fa-play-circle");
+const btnPlay = document.querySelector(".fa-play");
 const btnLoop = document.querySelector(".btn-loop");
 const btnNext = document.querySelector(".btn-next");
 const btnPrev = document.querySelector(".btn-prev");
 const btnRandom = document.querySelector(".btn-random");
 const audio = document.querySelector("#audio");
-const progress = document.querySelector(".progress");
+const progressBarTest = document.querySelector(".test-progress-bar");
 const timesong = document.querySelector(".duration-time");
-const progressBar = document.querySelector(".progress-bar");
 const currentTimeDisplay = document.querySelector(".current-time");
 const list = document.querySelector(".list-music");
 const songlist = list.getElementsByTagName("li");
@@ -26,7 +27,7 @@ let isRandom = false;
 let isLoop = false;
 let arraySongs = [];
 let base_api = "https://api.apify.com/v2/key-value-stores/EJ3Ppyr2t73Ifit64/records/LATEST/";
-fectRequest = async (url) =>{
+fectRequest = async (url) => {
 	const response = await fetch(url);
 	return response.json();
 }
@@ -37,23 +38,23 @@ const getData = async () => {
 	} else {
 		return Promise.reject(response.status);
 	}
-	
+
 };
-getSong = async()=>{
-	
+getSong = async () => {
+
 	const result = getData();
 	list.innerHTML = "";
-	result.then(data=>{
+	result.then(data => {
 		let all = data.songs.top100_VN;
 		let nt = all[0].songs;
-		for(let i = 0; i < nt.length; i++){
+		for (let i = 0; i < nt.length; i++) {
 			const name = nt[i].title;
 			const creator = nt[i].creator;
 			const music = nt[i].music;
 			const avatar = nt[i].avatar;
 			const bgImage = nt[i].bgImage;
 			list.innerHTML +=
-			`<li class="list-music-item" data-name='${name}' data-creator='${creator}' data-music='${music}'
+				`<li class="list-music-item" data-name='${name}' data-creator='${creator}' data-music='${music}'
 			data-avatar='${avatar}' data-index='${i}'>
 			<div class="list-music-item-info">
 			<span class="name">${name}</span>
@@ -65,15 +66,15 @@ getSong = async()=>{
 			</li>`
 		}
 		let songs = document.querySelectorAll(".list-music-item")
-		for(let i = 0; i < songs.length; i++){
+		for (let i = 0; i < songs.length; i++) {
 			arraySongs.push(songs[i].getAttribute("data-music"));
 		}
-		loadSong();	
-		
+		loadSong();
+
 	})
-	
+
 }
-formatTime = (second)=>{
+formatTime = (second) => {
 	let hours = Math.floor(second / 3600);
 	let minutes = Math.floor((second - hours * 3600) / 60);
 	let seconds = Math.floor(second - hours * 3600 - minutes * 60);
@@ -86,10 +87,10 @@ formatTime = (second)=>{
 	}
 	return (hours !== 0 ? hours + ':' : '') + minutes + ':' + seconds;
 }
-loadSong = async(song)=>{
+loadSong = async (song) => {
 	song = songIndex;
 	audio.src = arraySongs[song];
-	audio.addEventListener('loadedmetadata', ()=>{
+	audio.addEventListener('loadedmetadata', () => {
 		const time = formatTime(audio.duration);
 		timesong.textContent = time;
 	})
@@ -97,147 +98,164 @@ loadSong = async(song)=>{
 	name.textContent = detailSong[song].getAttribute("data-name");
 	creator.textContent = detailSong[song].getAttribute("data-creator");
 	avatar.src = detailSong[song].getAttribute("data-avatar");
-	for(let i = 0; i < songlist.length; i++){
+	for (let i = 0; i < songlist.length; i++) {
 		songlist[i].classList.remove("active");
 	}
 	songlist[song].classList.add("active");
-	songlist[song].scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+	songlist[song].scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest" });
 }
-function loader(){
+function loader() {
 	var loader = document.getElementById("wrap-pre-loader");
-	window.addEventListener("load",function(){
-	loader.style.display = "none";
+	window.addEventListener("load", function () {
+		loader.style.display = "none";
 	})
 }
-addLoopSong = () =>{
+addLoopSong = () => {
 	btnLoop.classList.remove('bx-remove-loop');
 	btnLoop.classList.add('bx-add-loop');
-	if(songIndex > arraySongs.length - 1){
+	if (songIndex > arraySongs.length - 1) {
 		songIndex = 0;
 	}
 }
-removeLoopSong = () =>{
+removeLoopSong = () => {
 	btnLoop.classList.add('bx-remove-loop');
 	btnLoop.classList.remove('bx-add-loop');
-	if(songIndex > arraySongs.length - 1){
+	if (songIndex > arraySongs.length - 1) {
 		songIndex = 0;
 	}
 }
-playSong = ()=>{
+playSong = () => {
 	musicContent.classList.add("playing");
 	avatar.style.animationPlayState = 'running';
-	btnPlay.classList.remove('fa-play-circle');
-	btnPlay.classList.add('fa-pause-circle');
+	btnPlay.classList.remove('fa-play');
+	btnPlay.classList.add('fa-pause');
 	audio.play();
 }
-pauseSong = ()=>{
+pauseSong = () => {
 	musicContent.classList.remove("playing");
 	avatar.style.animationPlayState = 'paused';
-	btnPlay.classList.add('fa-play-circle');
-	btnPlay.classList.remove('fa-pause-circle');
+	btnPlay.classList.add('fa-play');
+	btnPlay.classList.remove('fa-pause');
 	audio.pause();
 }
-addRandomSong = () =>{
+addRandomSong = () => {
 	songIndex = Math.floor(Math.random() * 101);
 	btnRandom.classList.remove('bx-remove-random');
 	btnRandom.classList.add('bx-add-random');
-	if(songIndex > arraySongs.length - 1){
+	if (songIndex > arraySongs.length - 1) {
 		songIndex = 0;
 	}
 }
-removeRandomSong = () =>{
+removeRandomSong = () => {
 	btnRandom.classList.add('bx-remove-random');
 	btnRandom.classList.remove('bx-add-random');
-	if(songIndex > arraySongs.length - 1){
+	if (songIndex > arraySongs.length - 1) {
 		songIndex = 0;
 	}
 }
-nextSong = ()=>{
+nextSong = () => {
 	songIndex++;
-	if(songIndex > arraySongs.length - 1){
+	if (songIndex > arraySongs.length - 1) {
 		songIndex = 0;
 	}
 	loadSong(songIndex);
 }
-prevSong = ()=>{
+prevSong = () => {
 	songIndex--;
-	if(songIndex < 0){
-		songIndex = arraySongs.length -1;
+	if (songIndex < 0) {
+		songIndex = arraySongs.length - 1;
 	}
 	loadSong(songIndex);
 }
-updateProgress = (e)=>{
+updateProgressTime = (e) => {
 	const { currentTime, duration } = e.srcElement;
-	const progressPercent = (currentTime / duration) * 100;
-	progressBar.style.width = `${progressPercent}%`;
 	currentTimeDisplay.textContent = formatTime(currentTime);
 }
-setProgress = (e)=>{
-	const width = e.offsetX;
-	const progress = e.currentTarget;
-	const progressBarWidth = (width / progress.clientWidth) * 100;
-	progressBar.style.width = `${progressBarWidth}%`;
-	let { duration } = audio;
-	audio.currentTime = (width * duration) / progress.clientWidth;
-}
-audio.addEventListener("timeupdate", updateProgress);
-progress.addEventListener("click", setProgress);
-audio.addEventListener("ended", ()=>{
-	if(isLoop == true)
-	{
+audio.addEventListener("timeupdate", updateProgressTime);
+
+audio.ontimeupdate = function () {
+	if (audio.duration) {
+		const progressPercent = (audio.currentTime / audio.duration) * 100;
+		progressBarTest.value = progressPercent;
+		var val = progressPercent;
+		if ((val >= 90) && (val <= 99)) val = (val - 1);
+		create_style("input[type=range]::-webkit-slider-runnable-track { background: linear-gradient(90deg, rgba(218,80,25,1) 0%, rgba(184,160,34,1) " + val + "%, #1D2021 " + val + "%, #1D2021 100%) !important;}");
+	}
+};
+progressBarTest.onchange = function (e) {
+	const seekTime = (audio.duration / 100) * e.target.value;
+	audio.currentTime = seekTime;
+	var val = e.target.value;
+	if ((val >= 90) && (val <= 99)) val = (val - 1);
+	create_style("input[type=range]::-webkit-slider-runnable-track { background: linear-gradient(90deg, rgba(218,80,25,1) 0%, rgba(184,160,34,1) " + val + "%, #1D2021 " + val + "%, #1D2021 100%) !important;}");
+};
+audio.addEventListener("ended", () => {
+	if (isLoop == true) {
 		playSong();
 	}
-	else if(isLoop == false)
-	{
+	else if (isLoop == false) {
 		nextSong();
 		playSong();
 	}
-	
 })
-btnPlay.addEventListener("click", ()=>{
-	if(musicContent.classList.contains("playing")){
+btnPlay.addEventListener("click", () => {
+	if (musicContent.classList.contains("playing")) {
 		pauseSong();
-	}else {
+	} else {
 		playSong();
 	}
 })
-btnNext.addEventListener("click", ()=>{
+btnNext.addEventListener("click", () => {
 	nextSong();
-	setTimeout(()=>{
+	setTimeout(() => {
 		playSong();
 	}, 400)
 })
-btnPrev.addEventListener("click", ()=>{
+btnPrev.addEventListener("click", () => {
 	prevSong();
-	setTimeout(()=>{
+	setTimeout(() => {
 		playSong();
 	}, 400)
 })
-btnRandom.addEventListener("click",()=>{
-	if(isRandom == false ){
+btnRandom.addEventListener("click", () => {
+	if (isRandom == false) {
 		isRandom = true;
 		addRandomSong();
 	}
-	else if(isRandom == true ){
+	else if (isRandom == true) {
 		isRandom = false;
 		removeRandomSong();
 	}
 })
-btnLoop.addEventListener("click",()=>{
-	if(isLoop == false ){
+btnLoop.addEventListener("click", () => {
+	if (isLoop == false) {
 		isLoop = true;
 		addLoopSong();
 	}
-	else if(isLoop == true ){
+	else if (isLoop == true) {
 		isLoop = false;
 		removeLoopSong();
 	}
 })
-list.addEventListener("click", (e)=>{
+list.addEventListener("click", (e) => {
 	songIndex = e.target.closest("li").getAttribute("data-index");
 	loadSong(songIndex);
 	playSong();
 })
-console.log(isLoader);
+function create_style(css) {
+	head = document.head,
+		oldstyles = head.querySelector("#rangestyle"),
+		style = document.createElement('style');
+	if (oldstyles != null) {
+		oldstyles.remove();
+	}
+	style.id = "rangestyle";
+	head.appendChild(style);
 
-
+	style.type = 'text/css';
+	if (style.styleSheet) {
+		style.styleSheet.cssText = css;
+	} else {
+		style.appendChild(document.createTextNode(css));
+	}
+}
