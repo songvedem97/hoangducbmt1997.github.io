71adfb491bb098adc32f3c2654824c4935e7b7ab
songvedem <56100101+hoangducbmt1997@users.noreply.github.com>
2021-10-07 13:09:43 +0700

    Update app.js

--- SVDMusic/assets/js/app.js
+++ SVDMusic/assets/js/app.js
@@ -101,12 +101,12 @@ loadSong = async () => {
 		  /*
 		  album: 'Whenever You Need Somebody',*/
 		  artwork: [
-			{ src: 'https://dummyimage.com/96x96',   sizes: '96x96',   type: 'image/png' },
-			{ src: 'https://dummyimage.com/128x128', sizes: '128x128', type: 'image/png' },
-			{ src: 'https://dummyimage.com/192x192', sizes: '192x192', type: 'image/png' },
-			{ src: 'https://dummyimage.com/256x256', sizes: '256x256', type: 'image/png' },
-			{ src: 'https://dummyimage.com/384x384', sizes: '384x384', type: 'image/png' },
-			{ src: 'https://dummyimage.com/512x512', sizes: '512x512', type: 'image/png' },
+			{ src: detailSong[song].getAttribute("data-avatar"),   sizes: '96x96',   type: 'image/png' },
+			{ src: detailSong[song].getAttribute("data-avatar"), sizes: '128x128', type: 'image/png' },
+			{ src: detailSong[song].getAttribute("data-avatar"), sizes: '192x192', type: 'image/png' },
+			{ src: detailSong[song].getAttribute("data-avatar"), sizes: '256x256', type: 'image/png' },
+			{ src: detailSong[song].getAttribute("data-avatar"), sizes: '384x384', type: 'image/png' },
+			{ src: detailSong[song].getAttribute("data-avatar"), sizes: '512x512', type: 'image/png' },
 		  ]
 		});
 	  
