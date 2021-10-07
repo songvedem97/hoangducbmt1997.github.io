72cefe9f9066beb16b2c034a822d14bd7943573a
songvedem <56100101+hoangducbmt1997@users.noreply.github.com>
2021-10-07 13:04:09 +0700

    Update app.js

--- SVDMusic/assets/js/app.js
+++ SVDMusic/assets/js/app.js
@@ -94,32 +94,30 @@ loadSong = async () => {
 	avatarControl.src=detailSong[song].getAttribute("data-avatar");
 	imgBlurSong.src=detailSong[song].getAttribute("data-img");
 	getLrc(detailSong[song].getAttribute('data-lrc'));
+	if ('mediaSession' in navigator) {
+		navigator.mediaSession.metadata = new MediaMetadata({
+		  title: detailSong[song].getAttribute("data-name"),
+		  artist: detailSong[song].getAttribute("data-creator"),
+		  /*
+		  album: 'Whenever You Need Somebody',*/
+		  artwork: [
+			{ src: 'https://dummyimage.com/96x96',   sizes: '96x96',   type: 'image/png' },
+			{ src: 'https://dummyimage.com/128x128', sizes: '128x128', type: 'image/png' },
+			{ src: 'https://dummyimage.com/192x192', sizes: '192x192', type: 'image/png' },
+			{ src: 'https://dummyimage.com/256x256', sizes: '256x256', type: 'image/png' },
+			{ src: 'https://dummyimage.com/384x384', sizes: '384x384', type: 'image/png' },
+			{ src: 'https://dummyimage.com/512x512', sizes: '512x512', type: 'image/png' },
+		  ]
+		});
+	  
+		navigator.mediaSession.setActionHandler('play', function() {});
+		navigator.mediaSession.setActionHandler('pause', function() {});
+		navigator.mediaSession.setActionHandler('previoustrack', function() {});
+		navigator.mediaSession.setActionHandler('nexttrack', function() {});
+	};
+	
 	
 }
-if ('mediaSession' in navigator) {
-	song = songIndex;
-	let detailSong = document.querySelectorAll(".list-music-item");
-
-    navigator.mediaSession.metadata = new MediaMetadata({
-      title: detailSong[song].getAttribute("data-name"),
-      artist: detailSong[song].getAttribute("data-creator"),
-	  /*
-      album: 'Whenever You Need Somebody',*/
-      artwork: [
-        { src: 'https://dummyimage.com/96x96',   sizes: '96x96',   type: 'image/png' },
-        { src: 'https://dummyimage.com/128x128', sizes: '128x128', type: 'image/png' },
-        { src: 'https://dummyimage.com/192x192', sizes: '192x192', type: 'image/png' },
-        { src: 'https://dummyimage.com/256x256', sizes: '256x256', type: 'image/png' },
-        { src: 'https://dummyimage.com/384x384', sizes: '384x384', type: 'image/png' },
-        { src: 'https://dummyimage.com/512x512', sizes: '512x512', type: 'image/png' },
-      ]
-    });
-  
-    navigator.mediaSession.setActionHandler('play', function() {});
-    navigator.mediaSession.setActionHandler('pause', function() {});
-    navigator.mediaSession.setActionHandler('previoustrack', function() {});
-    navigator.mediaSession.setActionHandler('nexttrack', function() {});
-};
 
 
 
