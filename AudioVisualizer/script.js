6b515a04f0227f7dfa2b0ce7e1dd24ae72bbf37f
songvedem <56100101+hoangducbmt1997@users.noreply.github.com>
2021-10-17 01:19:05 +0700

    add picture

--- AudioVisualizer/script.js
+++ AudioVisualizer/script.js
@@ -1,34 +1,134 @@
-const audioContext = new AudioContext(),
-  analyser = new AnalyserNode(audioContext, {
-    fftSize: 2048
+window.onload = async () => {
+  getSong();
+}
+
+
+
+const musicContent = document.querySelector(".container-app");
+const listSong = document.querySelector('.list-music');
+const timesong = document.querySelector(".duration-time");
+const currentTimeDisplay = document.querySelector(".current-time");
+var songIndex = 0;
+const audioContext = new AudioContext(), analyser = new AnalyserNode(audioContext, { fftSize: 2048 })
+const music = new Audio();
+const audio = document.querySelector("#audio");
+const scrubRuler = document.getElementById('scrub-ruler');
+const togglePlayPause = document.getElementById('toggle-play-pause');
+const btnNext = document.querySelector(".btn-next");
+const btnPrev = document.querySelector(".btn-prev");
+const source = audioContext.createMediaElementSource(music);
+var request = new XMLHttpRequest();
+var arraySongs = [];
+audio.volume = 0;
+
+
+getSong = async () => {
+  let songs = document.querySelectorAll(".list-music-item");
+  for (let i = 0; i < songs.length; i++) {
+    arraySongs.push(songs[i].getAttribute("data-music"));
+  }
+  loadSong();
+}
+
+loadSong = async () => {
+  song = songIndex;
+  audio.src = arraySongs[song];
+  audio.addEventListener('loadedmetadata', () => {
+    const time = formatTime(audio.duration);
+    timesong.textContent = time;
   })
-const music = new Audio(),
-  scrubRuler = document.getElementById('scrub-ruler'),
-  togglePlayPause = document.getElementById('toggle-play-pause'),
-  source = audioContext.createMediaElementSource(music)
+
+  request.open('GET', audio.src, true);
+  request.responseType = 'blob';
+  request.onload = function () {
+    music.src = window.URL.createObjectURL(request.response);
+    connectAudioToAnalyser(music, analyser, audioContext);
+  }
+  request.send();
+  draw();
+}
 
 
-music.crossOrigin = 'anonymous'
+formatTime = (second) => {
+  let hours = Math.floor(second / 3600);
+  let minutes = Math.floor((second - hours * 3600) / 60);
+  let seconds = Math.floor(second - hours * 3600 - minutes * 60);
+  hours = hours < 10 ? (hours > 0 ? '0' + hours : 0) : hours;
+  if (minutes < 10) {
+    minutes = '0' + minutes;
+  }
+  if (seconds < 10) {
+    seconds = '0' + seconds;
+  }
+  return (hours !== 0 ? hours + ':' : '') + minutes + ':' + seconds;
+}
+
+updateProgressTime = (e) => {
+  const { currentTime, duration } = e.srcElement;
+  currentTimeDisplay.textContent = formatTime(currentTime);
+}
+audio.addEventListener("timeupdate", updateProgressTime);
+
+
+nextSong = () => {
+
+	songIndex++;
+	if (songIndex > arraySongs.length - 1) {
+		songIndex = 0;
+	}
+	loadSong(songIndex);
+}
+prevSong = () => {
+	songIndex--;
+	if (songIndex < 0) {
+		songIndex = arraySongs.length - 1;
+	}
+	loadSong(songIndex);
+}
+
 music.addEventListener('ended', () => {
-  togglePlayPause.checked = false
+  togglePlayPause.checked = false;
 })
 
 music.addEventListener('timeupdate', () => {
-  scrubRuler.value = music.currentTime
+  scrubRuler.value = music.currentTime;
 })
 
 scrubRuler.addEventListener('change', event => {
-  music.currentTime = event.target.value
-  music.play()
-  togglePlayPause.checked = true
+  music.currentTime = event.target.value;
+  music.play();
+  togglePlayPause.checked = true;
 })
 
 togglePlayPause.addEventListener('change', event => {
-  if (event.target.checked)
-    music.play()
-  else
-    music.pause()
+  if (event.target.checked) {
+    music.play();
+    audio.play();
+  }
+
+  else {
+    music.pause();
+    audio.pause();
+  }
+
 })
+btnNext.addEventListener("click", () => {
+  console.log(a);
+	nextSong();
+	setTimeout(() => {
+		playSong();
+    music.play();
+	}, 400)
+})
+btnPrev.addEventListener("click", () => {
+  console.log(a);
+	prevSong();
+	setTimeout(() => {
+		playSong();
+    music.play();
+	}, 400)
+})
+
 
 document.addEventListener('keypress', event => {
   if (event.key === ' ') {
@@ -56,16 +156,6 @@ const connectAudioToAnalyser = (audioElement, analyserNode, context) => {
   }
   audioElement.addEventListener('canplay', canplayHandler)
 }
-var request = new XMLHttpRequest();
-
-request.open('GET', './alone.mp3', true);
-request.responseType = 'blob';
-
-request.onload = function () {
-  music.src = window.URL.createObjectURL(request.response);
-  connectAudioToAnalyser(music, analyser, audioContext);
-}
-request.send();
 
 function draw() {
   setTimeout(() => requestAnimationFrame(draw), 50)
@@ -91,5 +181,3 @@ function draw() {
   updateValue(section1, 20, 0.2)
 
 }
-
-draw();
