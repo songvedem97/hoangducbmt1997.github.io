93a9b837204c97d5e600394b3bc3e3e9b634e589
songvedem <56100101+hoangducbmt1997@users.noreply.github.com>
2021-10-19 02:38:38 +0700

    add new version

--- AudioVisualizer/script.js
+++ AudioVisualizer/script.js
@@ -7,6 +7,7 @@ var isPlay = false;
 var offsetHeight = document.getElementById('bar10').offsetHeight;
 avatar.style.animationPlayState = 'paused';
 var listSongs = new Array();
+var closeAudioContext = true;
 var i = 0;
 
 startApp();
@@ -21,8 +22,8 @@ function startApp() {
 		}
 	}
 	getSong();
-
-	var context = new (window.AudioContext || window.webkitAudioContext)();
+	// create web audio api context
+	var context = new (window.AudioContext || window.webkitAudioContext);
 	const analyser = context.createAnalyser();
 	const numPoints = analyser.frequencyBinCount;
 	const audioDataArray = new Uint8Array(numPoints);
@@ -32,7 +33,7 @@ function startApp() {
 			const ndx = ((x * numPoints) / 16) | 0;
 			const audioValue = audioDataArray[ndx] / 255;
 			const y = audioValue * 40;
-			if (x != 1) {
+			if (x != 9) {
 				document.getElementById("bar" + x).style.height = y + offsetHeight / 6 + "vh";
 				document.getElementById("bar" + x).style.width = y + offsetHeight / 6 + "vh";
 			}
@@ -46,22 +47,21 @@ function startApp() {
 	requestAnimationFrame(render);
 	var audio = new Audio();
 	audio.autoplay = false;
+	audio.src = listSongs[0];
 	audio.crossOrigin = "anonymous";
 	audio.addEventListener("canplay", handleCanplay);
-	audio.src = listSongs[0];
 	function handleCanplay() {
-
 		const source = context.createMediaElementSource(audio);
 		source.connect(analyser);
 		analyser.connect(context.destination);
 	}
-
 	playSong =()=>{
 		avatar.style.animationPlayState = 'running';
 		btnPlay.classList.remove('fa-play');
 		btnPlay.classList.add('fa-pause');
 		audio.play();
 		isPlay = true;
+		
 	}
 	pauseSong =()=>{
 		avatar.style.animationPlayState = 'paused';
@@ -69,6 +69,7 @@ function startApp() {
 		btnPlay.classList.remove('fa-pause');
 		audio.pause();
 		isPlay = false;
+		
 	}
 
 	btnPlay.addEventListener('click', () => {
@@ -82,11 +83,13 @@ function startApp() {
 	})
 	btnNext.addEventListener('click', () => {
 		i = ++i < listSongs.length ? i : 0;
+	
 		audio.src = listSongs[i];
 		playSong();
 	})
 	btnPrev.addEventListener('click', () => {
 		i = --i >= 0 ? i : listSongs.length-1;
+	
 		audio.src = listSongs[i];
 		playSong();
 	})
